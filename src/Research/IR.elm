7e04b100b39c12297983c4fecbe925f8173014b0
Ed Kelly <edwardjkelly@gmail.com>
2026-04-22 10:48:53 +0000

    Try something different with Research.IR

--- src/Research/IR.elm
+++ src/Research/IR.elm
@@ -1,16 +1,21 @@
 module Research.IR exposing (..)
 
+import Json.Decode as JD
+import Json.Encode as JE
+
+
+
 -- example
 
 
 type Semaphore
     = Yellow
-    | Green Bool
-    | Red Bool String
+    | Green Bool String
+    | Red Bool Bool
 
 
-example : IR Semaphore
-example =
+semaphoreCodec : IRCodec Semaphore Semaphore
+semaphoreCodec =
     custom
         (\red yellow green value ->
             case value of
@@ -20,80 +25,232 @@ example =
                 Yellow ->
                     yellow
 
-                Green f ->
-                    green f
+                Green f x ->
+                    green f x
         )
-        |> variant2 Red bool string
+        |> variant2 Red bool bool
         |> variant0 Yellow
-        |> variant1 Green bool
+        |> variant2 Green bool string
         |> endCustom
+
+
+encodeSemaphore : Semaphore -> JE.Value
+encodeSemaphore s =
+    encode semaphoreCodec s
+
+
+semaphoreDecoder : JD.Decoder Semaphore
+semaphoreDecoder =
+    decoder semaphoreCodec
+
+
+encode : IRCodec b b -> b -> JE.Value
+encode codec value =
+    codec.toIRType value
+        |> encodeAdapter
+
+
+decoder : IRCodec b b -> JD.Decoder b
+decoder codec =
+    decodeAdapter
+        |> JD.andThen
+            (\ir ->
+                case codec.fromIRType ir of
+                    Ok s ->
+                        JD.succeed s
+
+                    Err _ ->
+                        JD.fail ""
+            )
 
 
 
 -- innards
 
 
-type IR a
-    = IR
-        { toIRType : a -> IRType
-        }
+type Error
+    = Error
 
 
-type IRType
+type alias IRCodec a b =
+    { toIRType : a -> IR
+    , fromIRType : IR -> Result Error b
+    }
+
+
+type IR
     = Bool Bool
     | String String
       -- | Product (List IRType)
-    | Custom Int (List IRType)
+    | Custom Int Variant
 
 
-bool : IR Bool
+encodeAdapter irType =
+    case irType of
+        Bool b ->
+            JE.object
+                [ ( "bool", JE.bool b ) ]
+
+        String s ->
+            JE.object
+                [ ( "string", JE.string s ) ]
+
+        Custom selected variant ->
+            JE.object
+                [ ( "custom"
+                  , JE.object
+                        [ ( "tag", JE.int selected )
+                        , ( "args"
+                          , JE.list encodeAdapter
+                                (case variant of
+                                    Variant0 ->
+                                        []
+
+                                    Variant1 arg ->
+                                        [ arg ]
+
+                                    Variant2 arg1 arg2 ->
+                                        [ arg1
+                                        , arg2
+                                        ]
+                                )
+                          )
+                        ]
+                  )
+                ]
+
+
+decodeAdapter =
+    JD.oneOf
+        [ JD.field "bool" JD.bool |> JD.map Bool
+        , JD.field "string" JD.string |> JD.map String
+        , JD.field "custom"
+            (JD.map2
+                (\selected args ->
+                    case args of
+                        [] ->
+                            Just (Custom selected Variant0)
+
+                        [ arg ] ->
+                            Just (Custom selected (Variant1 arg))
+
+                        [ arg1, arg2 ] ->
+                            Just (Custom selected (Variant2 arg1 arg2))
+
+                        _ ->
+                            Nothing
+                )
+                (JD.field "tag" JD.int)
+                (JD.field "args" (JD.list (JD.lazy (\_ -> decodeAdapter))))
+                |> JD.andThen
+                    (\mc ->
+                        case mc of
+                            Nothing ->
+                                JD.fail ""
+
+                            Just c ->
+                                JD.succeed c
+                    )
+            )
+        ]
+
+
+type Variant
+    = Variant0
+    | Variant1 IR
+    | Variant2 IR IR
+
+
+bool : IRCodec Bool Bool
 bool =
-    IR { toIRType = Bool }
+    { toIRType = Bool
+    , fromIRType =
+        \ir ->
+            case ir of
+                Bool b ->
+                    Ok b
+
+                _ ->
+                    Err Error
+    }
 
 
-string : IR String
+string : IRCodec String String
 string =
-    IR { toIRType = String }
+    { toIRType = String
+    , fromIRType =
+        \ir ->
+            case ir of
+                String s ->
+                    Ok s
+
+                _ ->
+                    Err Error
+    }
 
 
-custom : match -> { match : match, index : Int }
 custom match =
-    { match = match, index = 0 }
+    { match = match
+    , index = 0
+    , fromIRType = \_ -> Err Error
+    }
 
 
-variant0 :
-    variant
-    -> { match : IRType -> match, index : Int }
-    -> { match : match, index : Int }
 variant0 ctor prev =
-    { match = prev.match <| Custom prev.index []
+    { match = prev.match <| Custom prev.index Variant0
     , index = prev.index + 1
+    , fromIRType =
+        \ir ->
+            case ir of
+                Custom selected Variant0 ->
+                    if selected == prev.index then
+                        Ok ctor
+
+                    else
+                        prev.fromIRType ir
+
+                _ ->
+                    prev.fromIRType ir
     }
 
 
-variant1 :
-    (arg -> variant)
-    -> IR arg
-    -> { match : (arg -> IRType) -> match, index : Int }
-    -> { match : match, index : Int }
-variant1 ctor (IR argfns) prev =
-    { match = prev.match <| \arg -> Custom prev.index [ argfns.toIRType arg ]
+variant1 ctor argfns prev =
+    { match = prev.match <| \arg -> Custom prev.index (Variant1 (argfns.toIRType arg))
     , index = prev.index + 1
+    , fromIRType =
+        \ir ->
+            case ir of
+                Custom selected (Variant1 arg) ->
+                    if selected == prev.index then
+                        Result.map ctor (argfns.fromIRType arg)
+
+                    else
+                        prev.fromIRType ir
+
+                _ ->
+                    prev.fromIRType ir
     }
 
 
-variant2 :
-    (arg1 -> arg2 -> variant)
-    -> IR arg1
-    -> IR arg2
-    -> { match : (arg1 -> arg2 -> IRType) -> match, index : Int }
-    -> { match : match, index : Int }
-variant2 ctor (IR arg1fns) (IR arg2fns) prev =
-    { match = prev.match <| \arg1 arg2 -> Custom prev.index [ arg1fns.toIRType arg1, arg2fns.toIRType arg2 ]
+variant2 ctor arg1fns arg2fns prev =
+    { match = prev.match <| \arg1 arg2 -> Custom prev.index (Variant2 (arg1fns.toIRType arg1) (arg2fns.toIRType arg2))
     , index = prev.index + 1
+    , fromIRType =
+        \ir ->
+            case ir of
+                Custom selected (Variant2 arg1 arg2) ->
+                    if selected == prev.index then
+                        Result.map2 ctor (arg1fns.fromIRType arg1) (arg2fns.fromIRType arg2)
+
+                    else
+                        prev.fromIRType ir
+
+                _ ->
+                    prev.fromIRType ir
     }
 
 
-endCustom : { match : a -> IRType, index : Int } -> IR a
 endCustom prev =
-    IR { toIRType = prev.match }
+    { toIRType = prev.match
+    , fromIRType = prev.fromIRType
+    }
